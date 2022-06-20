96a711fc40b5596e36f17fc1cbb2548f7008b0a7
SandeMiriam <miriamsandeoyuga@gmail.com>
2022-06-20 19:01:55 +0300

    first commit

--- assignment.py
+++ assignment.py
@@ -1,38 +1,95 @@
+from datetime import datetime
+
+
 class Account:
-    def __init__(self,name,account_number):
+    def __init__(self,account_name, account_number):
         self.balance=0
-        self.name=name
+        self.account_name=account_name
         self.account_number=account_number
-        self.deposits = []
-        self.withdrawals =[]
-        self.transctions=100
+        self.deposits=[]
+        self.withdrawals=[]
+        self.transaction= 100
+        self.date=datetime.now()
+        self.loan_balance=0
         
+
         
     def deposit(self,amount):
-        if amount<=0:
+        if amount <=0:
             return f"Deposit amount should be more than zero"
         else:
             self.balance += amount
-            self.deposits.append(f"You have deposited{amount}")
-            return f"You have deposited {amount}. Your new balance is {self.balance}. {self.deposits}"  
-
-    def withdraw(self,amount):
-        if amount>self.balance:
-             return f"Your balance is {self.balance}, you cannot withdraw the {amount}" 
-        elif amount<=0:
-            return f"Amount must be greater than zero" 
-        else:
-            self.balance-=amount+self.transctions
-            self.withdrawals.append(f"you have withdraw{amount}")
-            return f"you have withdraw {amount} your balance is {self.balance}. {self.withdrawals}" 
+            self.deposits.append({"date":self.date.strftime('%c'), "amount":amount,"narration":"deposit"})
+            return f"You deposited {amount}.Your new balance is {self.balance}"
+             
     
-def deposits_statement(self):
-        for statements in self.deposits:
-            print(statements)
-def withdraws_statement(self):
-        for staments in self.withdraws:
-            print(staments)
-def current_balance(self):
-        balance = self.balance
-        print(balance)
+    def withdraw(self, amount):
+        if (amount+self.transaction)>self.balance:
+            return f"Your balance is {self.balance}, you cannot withdraw the {amount}"
+        elif amount<=0:
+            return f"Amount must be greater than zero"
+        else:    
+            self.balance-=(amount +self.transaction)
+            self.withdrawals.append({"date":self.date.strftime('%c'), "amount":amount,"narration":"Withdrawn"})
+            return f"You withdrew {amount} and the transaction cost was {self.transaction}.Your new balane is {self.balance}"
+        
+    def deposits_statement(self):
+        for depo in self.deposits:
+            print(depo)
+        
+    def withdrawals_statement(self):
+        for withdraws in self.withdrawals:
+            print(withdraws) 
+            
+    def current_balance(self):
+        balance=self.balance
+        print("Your current balance is {balance}")  
+        
+    def full_statement(self):
+        statements= (*self.deposits,*self.withdrawals)    
+        for statement in statements:
+            print(statement)  
+            
+    def borrow(self,amount): 
+        
+        sum=0
+        for depo in self.deposits:
+            sum+=depo["amount"]
+        if (len(self.deposits)) <10:
+            print(f"Can't be lended this {amount} you've deposited less than 10 times.") 
+        elif(amount <100):
+            print("You can't borrow  less than 100") 
+        elif amount > (sum//3):
+            print("You've exceeded the loan limit")
+        elif self.balance==0:
+            print("You can't borrow.Your account balance is 0")    
+        elif self.loan_balance>0:
+            print(f"You have an outstanding loan of {self.loan_balance}.You can't borrow this {amount}")    
+        else:
+            self.loan_balance+=(amount+(amount*0.03))
+            print(f"Your loan is {amount} and an interest of{amount*0.03} and total loan amount is {self.loan_balance}")    
+            
+    def loan_repayment(self,amount):
+        overpayment=amount-self.loan_balance
+        loan_payment=amount-overpayment
+        if amount<0:
+            print(f"Repayment amount has to be more than 0")
+        elif amount <= self.loan_balance:
+            self.loan_balance-=amount
+            print(f"Your loan balance is {self.loan_balance} ")    
+        else:
+            self.loan_balance-=loan_payment
+            self.balance+=overpayment
+            print(f"You have an overpayment of {overpayment}") 
+    
+    def transfer(self, amount, acc2):
+        if amount > self.balance:
+            print("You don't have sufficient amount of money to transfer this amount.")
+        elif amount<=0:
+            print("Put a valid amount of money.")
+        else:    
+            self.balance-=amount
+            acc2.balance+=amount
+            print(f"{self.account_name} you've sent{amount} to {acc2.account_name}") 
+
 
---
+++ lists.py
@@ -0,0 +1,53 @@
+from re import X
+
+
+# list1=[100,200,300,400,500]
+# list1.reverse()
+# print(list1)
+# rev_list=list1[::-1]
+# print(rev_list)
+
+# swapping
+# list1[0],list1[-1]=list1[-1],list1[0]
+# print(list1)
+
+# listx=["m","na","i","Ke"]
+# listy=["y","me","s","lly"]
+# list3=[i+j,for i in listx]
+
+# square
+
+# comprehension
+# a=[2,4,3]
+# b=[i**2 for i in a]
+# print(b)
+
+# without
+# for i in a:
+#     print(i**2)
+# output in a list
+# b=[]
+# for i in a:
+#     b.append(i**2)
+# print(b)
+
+# removing an item in a list
+# a.pop()
+# print(a)
+
+# # adding an item to a list
+# a.append(5)
+# print(a)
+
+# use of .remove
+# a.remove(0)
+# print (a)
+
+#squre of a dictionary
+# def dict():
+#     x={i:i**2 for i in range(1,15)}
+#     print(x)
+# dict()
+
+# append
+ 
